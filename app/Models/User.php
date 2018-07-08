2583e490592ebd57e5fd1cc4d7abe1932833e21a
Jimmy Grimm <jimmygrimm1943@gmail.com>
2018-07-08 15:31:29 +0800

    List topics created by user

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,4 +21,14 @@ class User extends Authenticatable
     {
         return $this->id == $model->user_id;
     }
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
